9896734bd02e98d4b202d086be9d5fb616971f00
jialin <1072989580@qq.com>
2019-12-28 17:27:12 +0800

    format update

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -5,10 +5,10 @@ public class Rover {
     private String direction;
 
     public void land(Area area, int x, int y, String direction)  {
-        if (x > area.getX()){
+        if (x > area.getX()) {
             throw new IllegalArgumentException("x=" + x + "is out of area width" + area.getX());
         }
-        if (y > area.getY() ){
+        if (y > area.getY()) {
             throw new IllegalArgumentException("y=" + y + "is out of area width" + area.getY());
         }
         this.area = area;
@@ -18,16 +18,16 @@ public class Rover {
     }
 
     public String getPosition() {
-        return ""+ x + y + direction;
+        return "" + x + y + direction;
     }
 
     public void move() {
-        if (direction.equals("E") ) {
-            x +=1;
-        } else if (direction.equals("W") ) {
+        if (direction.equals("E")) {
+            x += 1;
+        } else if (direction.equals("W")) {
             x -= 1;
-        } else if (direction.equals("N") ){
-            y +=1;
+        } else if (direction.equals("N"))  {
+            y += 1;
         } else {
             y -= 1;
         }
@@ -35,11 +35,11 @@ public class Rover {
     }
 
     public void turnleft() {
-        if (direction.equals("E") ) {
+        if (direction.equals("E")) {
             direction = "N";
-        } else if (direction.equals("W") ) {
+        } else if (direction.equals("W")) {
             direction = "S";
-        } else if (direction.equals("N") ){
+        } else if (direction.equals("N")){
             direction = "W";
         } else {
             direction = "E";
@@ -48,11 +48,11 @@ public class Rover {
     }
 
     public void turnright() {
-        if (direction .equals("E") ) {
+        if (direction .equals("E")) {
             direction = "S";
-        } else if (direction.equals("W") ) {
+        } else if (direction.equals("W")) {
             direction = "N";
-        } else if (direction.equals("N") ){
+        } else if (direction.equals("N")){
             direction = "E";
         } else {
             direction = "W";
--- src/main/java/RoverController.java
+++ src/main/java/RoverController.java
@@ -11,13 +11,13 @@ public class RoverController {
 
        // String mission = "10,10,5,5,E,M,L,M";
         String[] command = mission.split(",");
-        Area area = new Area(Integer.parseInt(command[0]),Integer.parseInt(command[1]));
+        Area area = new Area(Integer.parseInt(command[0]), Integer.parseInt(command[1]));
 
-        rover.land(area,Integer.parseInt(command[2]),Integer.parseInt(command[3]),command[4]);
-        for (int i = 5; i <command.length ; i++) {
-            if (command[i] .equals("L") ){
+        rover.land(area,Integer.parseInt(command[2]), Integer.parseInt(command[3]), command[4]);
+        for (int i = 5;  i < command.length; i++) {
+            if (command[i] .equals("L")) {
                 rover.turnleft();
-            } else if (command[i].equals("R") ) {
+            } else if (command[i].equals("R")) {
                 rover.turnright();
             } else {
                 rover.move();
